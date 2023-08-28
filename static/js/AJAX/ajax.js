9187707ef5069ec9a4569424b2c21d20b7ed0651
GabJS10 <gballesteros@uniguajira.edu.co>
2023-08-27 19:34:22 -0500

    Comence API Fetch

---
+++ static/js/AJAX/ajax.js
@@ -0,0 +1,53 @@
+(() => {
+     const xhr = new XMLHttpRequest(),
+      $xhlol = document.getElementById("xhl"),
+       $fragment = document.createDocumentFragment()
+
+     xhr.addEventListener("readystatechange", e => {
+          if (xhr.readyState !== 4) return;
+
+          if(xhr.status >=200 && xhr.status<300){
+               let json = JSON.parse(xhr.responseText)
+
+               json.forEach(user => {
+                    const $li = document.createElement("li")
+                    $li.innerHTML = `${user.name} ---- ${user.email} ---- ${user.phone}`
+                    let $clone = document.importNode($li,true)
+                    $fragment.appendChild($clone)
+               });
+               
+               $xhlol.appendChild($fragment)
+
+          }else{
+               let message = xhr.statusText || "Ocurrio un error"
+               $xhlol.innerHTML = `Code: ${xhr.status}. ${message}`
+          }
+     })
+     xhr.open("GET","https://jsonplaceholder.typicode.com/users")
+     xhr.send()
+})();
+
+(() => {
+     const $fetchol = document.getElementById("fetch"),
+      $fragment = document.createDocumentFragment()
+
+     fetch("https://jsonplaceholder.typicode.com/users")
+     .then(res => res.ok ? res.json() : Promise.reject(res))
+     .then(json => {
+          json.forEach(user => {
+               const $li = document.createElement("li")
+               $li.innerHTML = `${user.name} ---- ${user.email} ---- ${user.phone}`
+               let $clone = document.importNode($li,true)
+               $fragment.appendChild($clone)
+          });
+          $fetchol.appendChild($fragment)
+     })
+     .catch(err => {
+          console.log(err);
+          let message = err.statusText || "Ocurrio un error"
+          $fetchol.innerHTML = `Code: ${err.status}. ${message}`
+     })
+     .finally(() => {
+          console.log("Este codigo se ejecuta de todos modos");
+     });
+})();
