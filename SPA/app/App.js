eab045eb688126f95e0d2b8ceb6823b106b6afe2
GabJS10 <gballesteros@uniguajira.edu.co>
2023-10-08 18:03:31 -0500

    SPA JS Vanilla

---
+++ SPA/app/App.js
@@ -0,0 +1,8 @@
+import API from "../app/helpers/wp_api.js"
+
+export function App() {
+     document.getElementById("root").innerHTML = "<h1>Bienvenido a mi primer SPA</h1>"
+
+     console.log(API);
+}
+
